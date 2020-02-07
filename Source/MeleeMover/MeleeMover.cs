4eaf278cd1dbe0770a572924c361648895f9aaad
Mad <github@typ03.de>
2020-02-07 10:10:27 +0100

    bump version

--- Source/MeleeMover/MeleeMover.cs
+++ Source/MeleeMover/MeleeMover.cs
@@ -14,7 +14,7 @@ namespace MeleeMover
         internal static string ModDirectory;
 
         // BEN: DebugLevel (0: nothing, 1: error, 2: debug, 3: info)
-        internal static int DebugLevel = 2;
+        internal static int DebugLevel = 1;
 
         public static void Init(string directory, string settings)
         {
