e9c5409353df21cfcdb27e141fedac2871c5269c
Mad <github@typ03.de>
2020-09-22 10:45:50 +0200

    opt build

--- Source/MeleeMover/Properties/AssemblyInfo.cs
+++ Source/MeleeMover/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("MeleeMover")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0D62834E-ACAD-472F-8A74-3E3B06D6643E")]
-[assembly: AssemblyVersion("1.8.1.*")]
+[assembly: AssemblyVersion("1.9.1.*")]
